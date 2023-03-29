66680f5a1ba5415bd85559a6f42f4f5f65dadaa2
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-28 21:53:25 -0300

    friend request scheme

---
+++ src/datebaseSchemas/friendRequest.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const schemaFriendRequest = mongoose.Schema({
+
+    user: { type: String, tim: true, required: true },
+    receiver: { type: String, tim: true, required: true },
+    
+})
+
+export default mongoose.model("friendRequest", schemaFriendRequest);
