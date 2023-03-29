2d883a83b8ddc8e8da455840f01354f7a2f626e6
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-28 21:48:12 -0300

    grouping all routes

---
+++ router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+const router = Router();
+
+import friendRouter from "@modules/friend/friendRouter.js";
+import usertRouter from "@modules/user/userRouter.js";
+import talkRouter from "@modules/talk/talkRouter.js";
+
+router.use("/user", usertRouter);
+router.use("/user/talk", talkRouter);
+router.use("/user/friend", friendRouter);
+
+export { router };
